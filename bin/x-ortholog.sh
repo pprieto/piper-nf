16e84b23b4cf0adf50d5dafc31ef4c84fa2616c3
Pablo Prieto <pablo.prieto.barja@gmail.com>
2013-11-12 21:34:15 +0100

    Adding x-ortholog.sh - blast for ortholog finding

--- bin/x-ortholog.sh
+++ bin/x-ortholog.sh
@@ -7,22 +7,20 @@
 # - $1: Blast strategy to use
 # - $2: Blast DB
 # - $3: Query file
-# - $4: Blast Gtf (aka reference gtf)
 
 set -e
 set -u
 
 case "$1" in
 'ncbi-blast')
-blastn -db $2/db -query $3 -max_target_seqs 1 -num_alignments 1 -max_hsps_per_subject 1 -num_threads 1  -outfmt '6 qseqid sseqid evalue score qgi bitscore length nident positive mismatch pident ppos qacc gaps gaopen qaccver qlen qframe qstart qend sframe sstart send'
+blastn -db $2/db -query $3 -evalue 0.00001 -word_size 7 -max_target_seqs 1 -max_hsps_per_subject 1 -num_threads 1  -outfmt '6 qseqid sseqid evalue score qgi bitscore length nident positive mismatch pident ppos qacc gaps gaopen qaccver qlen qframe qstart qend sstrand sstart send'
 ;;
 
 'wu-blast')
-wu-blastn -mformat=2 -V=1 -B=1 -spoutmax=1 -warnings -errors -notes
+wu-blastn $2/db $3 -mformat=2 -V=1 -B=1 -spoutmax=1 -warnings -errors -notes W=7 M=5 N=-4 Q=20 R=10 -e 0.00001 -cpus 1 -filter=seg -lcfilter
 ;;
 
 *) echo "Not a valid BLAST strategy: $1"; exit 1
 ;;
 
 esac
-
